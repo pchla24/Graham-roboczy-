b32bd978ba9465468744ca030d61bdc288d05889
pchla24 <janusz_sawicki97@o2.pl>
2017-10-29 11:57:39 +0100

    Nie cofa więcej niż 1 zły wybór

--- Otoczka/src/otoczka/MainHull.java
+++ Otoczka/src/otoczka/MainHull.java
@@ -4,19 +4,25 @@ public class MainHull {
 
 	public static void main(String[] args) {
 		
-		int size = 5;
+		int size = 8;
 		Points points = new Points(size);
 		
-		Point p1 = new Point(3, 3);
+		Point p1 = new Point(17, 3);
 		points.add(p1);
-		Point p2 = new Point(2, 2);
+		Point p2 = new Point(20, 9);
 		points.add(p2);
-		Point p3 = new Point(7, 7);
+		Point p3 = new Point(15, 10);
 		points.add(p3);
-		Point p4 = new Point(1, 1);
+		Point p4 = new Point(12, 13);
 		points.add(p4);
-		Point p5 = new Point(9, 9);
+		Point p5 = new Point(6, 9);
 		points.add(p5);
+		Point p6 = new Point(5, 12);
+		points.add(p6);
+		Point p7 = new Point(1, 6);
+		points.add(p7);
+		Point p8 = new Point(15, 6);
+		points.add(p8);
 		
 		//points.mergeSort();
 		//points.printPoints();
