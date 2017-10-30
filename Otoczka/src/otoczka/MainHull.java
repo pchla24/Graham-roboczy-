1983939a079ebbbb4a1876669c807198fabbb4d9
Janusz <Janusz@10.68.29.231>
2017-10-30 10:57:44 +0100

    Dodane czytanie z pliku

--- Otoczka/src/otoczka/MainHull.java
+++ Otoczka/src/otoczka/MainHull.java
@@ -1,35 +1,35 @@
 package otoczka;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MainHull {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		int size = 8;
+		File file = new File("points.txt");
+		Scanner scanner = new Scanner(file);
+		
+		int size = scanner.nextInt();
 		Points points = new Points(size);
 		
-		Point p1 = new Point(17, 3);
-		points.add(p1);
-		Point p2 = new Point(20, 9);
-		points.add(p2);
-		Point p3 = new Point(15, 10);
-		points.add(p3);
-		Point p4 = new Point(12, 13);
-		points.add(p4);
-		Point p5 = new Point(6, 9);
-		points.add(p5);
-		Point p6 = new Point(5, 12);
-		points.add(p6);
-		Point p7 = new Point(1, 6);
-		points.add(p7);
-		Point p8 = new Point(15, 6);
-		points.add(p8);
+		for(int i=0; i<size; i++) {
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+			Point p = new Point(x, y);
+			points.add(p);
+		}
+		
+		//points.printPoints();
+		
 		
 		//points.mergeSort();
 		//points.printPoints();
 
 		points.doGrahamScan();
 		
-		//points.ptsS.printStack();
+		//points.ptsS.printStack();*/
 	}
 
 }
